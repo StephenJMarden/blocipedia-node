9bd0c6649a62388c593764cd19aa108185cee49b
StephenJMarden <stephen.j.marden@gmail.com>
2018-12-14 20:41:12 -0500

    sign-in-out wip

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,12 @@
 const   express = require('express'),
         router = express.Router(),
-        userController = require('../controllers/userController');
+        userController = require('../controllers/userController'),
+        validation = require('./validation');
 
 router.get('/users/signup', userController.signUp);
-router.post('/users', userController.create);
+router.post('/users', validation.validateUsers, userController.create);
+router.get('/users/signin', userController.signInForm);
+router.post('/users/signin', validation.validateUsers, userController.signIn);
+router.get('/users/signout', userController.signOut);
 
 module.exports = router;
